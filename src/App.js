a7c862c75fe5f09cdc70adfaff59b98396686b0d
Vishva <adharsh.scipy@gmail.com>
2025-09-01 15:34:19 +0530

    vishva commit- 1

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 
+import PricingPage from "./pages/Pricing.jsx";
+
 function App() {
- 
+
   const getInitialTheme = () => {
     const savedTheme = localStorage.getItem("theme");
     if (savedTheme) return savedTheme;
@@ -18,11 +20,11 @@ function App() {
   const [theme, setTheme] = useState(getInitialTheme);
 
   useEffect(() => {
-  
+
     document.documentElement.setAttribute("data-theme", theme);
   }, [theme]);
 
-  
+
   const handleThemeChange = (newTheme) => {
     setTheme(newTheme);
     localStorage.setItem("theme", newTheme);
@@ -30,16 +32,16 @@ function App() {
 
   return (
     <div className="App">
-     
+
       <Header theme={theme} setTheme={handleThemeChange} />
 
       <Routes>
-        <Route path="/pricing" element={<h1>Pricing Page</h1>} />
+        <Route path="/pricing" element={<PricingPage />} />
         <Route path="/careers" element={<h1>Careers Page</h1>} />
         <Route path="/enterprise" element={<h1>Enterprise Page</h1>} />
         <Route path="/help-center" element={<h1>Help Center</h1>} />
       </Routes>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
